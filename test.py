f7b1c6fe842a44408333f01859b22318f222efa8
zous910814 <zous910814@gmail.com>
2022-07-12 19:18:11 +0800

    feat(main):change headers position & add date function

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+import datetime
 import time
-a = time.strftime('%Y%m%d', time.localtime())
-print(type(a))
+
+
+def get_date(days: int) -> list:
+    begin_date = (datetime.datetime.now() - datetime.timedelta(days=days - 1)).strftime("%Y-%m-%d")
+    date_list = []
+    begin_date = datetime.datetime.strptime(begin_date, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(time.strftime('%Y-%m-%d', time.localtime(time.time())), "%Y-%m-%d")
+    while begin_date <= end_date:
+        date_str = begin_date.strftime("%Y%m%d")
+        date_list.append(date_str)
+        begin_date += datetime.timedelta(days=1)
+    return date_list
+
+
+a = get_date(6)
+print(a[-1])
